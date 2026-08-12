1e1c5c6ebeda5bbd381ac1b322be121b20c40cae
comaboi51 <rosberoqe30@wvc.pshs.edu.ph>
2026-08-12 09:16:19 +0800

    Add files via upload

---
+++ diagnostic1_mg_rosbero.py
@@ -0,0 +1,20 @@
+earth_weight = input("what is ur weight on earth (in kg)?")
+destination = input("where r u going? Mars, Jupiter or the Moon?")
+
+def calculate_space_weight(earth_weight, destination):
+    global(earth_weight)
+    global(destination)
+    Mars = 0.38
+    Jupiter = 2.34
+    Moon = 0.16
+    if destination == 'Mars':
+        return (earth_weight*Mars)
+    elif destination == 'Jupiter':
+        return (earth_weight*Jupiter)
+    elif destination == 'Moon':
+        return (earth_weight*Moon)
+    else:
+        print("ERROR")
+        return None
+
+calculate_space_weight(earth_weight,destination)
